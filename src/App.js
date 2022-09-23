a873b355fd2b876dd2b62f1da5ea23333c8b4223
Jaden Garrity <110641058+MrTowelGuy@users.noreply.github.com>
2022-09-22 20:39:22 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Dog Giphy App</h1>
+      <h2>Let's see your pupper!</h2>
+    </div>
+  );
+}
